fc890de3cc681a32a99174c3b64ba9fb696b496a
mum <mumum3521@naver.com>
2024-04-30 13:51:10 +0900

    Feat : 가맹점 수정폼 수정

--- xcape-admin/src/main/resources/static/js/merchant.js
+++ xcape-admin/src/main/resources/static/js/merchant.js
@@ -1,6 +1,27 @@
 const getMerchantInformation = (e) => {
     const id = e.currentTarget.dataset.merchantId;
-    console.log(id)
+    axios.get(`/merchants/${id}`).then((res)=> {
+        const {resultCode} = res.data;
+        const merchant = res.data.result;
+        if (resultCode === SUCCESS) {
+            document.merchantInfo.action = `/merchants/${merchant.id}`;
+            document.querySelector('#modifyMerchantName').value = merchant.name;
+            document.querySelector('#modifyTelNumber').value = merchant.telNumber;
+            document.querySelector('#modifyAddress').value = merchant.address;
+            document.querySelector('#modifyBusinessHour').value = merchant.businessHour;
+            document.querySelector('#modifyCeoName').value = merchant.ceoName;
+            document.querySelector('#modifyBusinessRegistrationNumber').value = merchant.businessRegistrationNumber;
+            document.querySelector('#modifyEmail').value = merchant.email;
+            document.querySelector('#modifyCode').value = merchant.code;
+            document.querySelector('input[name="modifyParkingYn"]').checked = merchant.parkingYn;
+            document.querySelector('input[name="modifyUseYn"]').checked = merchant.useYn;
+            document.querySelector('#modifyBrandInfoNotionId').value = merchant.brandInfoNotionId;
+            document.querySelector('#modifyUsingInfoNotionId').value = merchant.usingInfoNotionId;
+            document.querySelector('#modifyAddressInfoNotionId').value = merchant.addressInfoNotionId;
+
+
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function(){
