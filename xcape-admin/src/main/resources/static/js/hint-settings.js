e52dd11597b376e5712e885a8f4f59bfcb30db14
mum <mumum3521@naver.com>
2024-06-07 23:07:58 +0900

    Refactor : 힌트목록 조회 js 리팩토링

--- xcape-admin/src/main/resources/static/js/hint-settings.js
+++ xcape-admin/src/main/resources/static/js/hint-settings.js
@@ -2,20 +2,18 @@ const getHintList = (e) => {
     const id = e.currentTarget.dataset.themeId;
     axios.get(`/themes/${id}/hints`).then((res) => {
         const {resultCode} = res.data;
-        const hintList = res.data.result;
         if (resultCode === SUCCESS) {
-            const tbody = document.querySelector('#hintTable tbody')
-            tbody.innerHTML = '';
-            hintList.forEach(hint => {
-                const {id, code, message1, message2, isUsed} = hint;
-                let html = document.querySelector('#hintTemplate').innerHTML;
-                tbody.innerHTML += html
-                    .replaceAll('{id}', id)
-                    .replaceAll('{code}', code)
-                    .replaceAll('{message1}', message1)
-                    .replaceAll('{message2}', message2)
-                    .replaceAll('{isUsed}', isUsed);
-            });
+            const tbody = document.querySelector('#hintTable tbody');
+            const template = document.querySelector('#hintTemplate').innerHTML;
+
+            tbody.innerHTML = res.data.result.map(hint => {
+                return template
+                    .replaceAll('{id}', hint.id)
+                    .replaceAll('{code}', hint.code)
+                    .replaceAll('{message1}', hint.message1)
+                    .replaceAll('{message2}', hint.message2)
+                    .replaceAll('{isUsed}', hint.isUsed ? '사용' : '미사용');
+            }).join('');
         }
     });
 }
