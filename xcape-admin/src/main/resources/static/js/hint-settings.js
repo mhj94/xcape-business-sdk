03cf024e952a1cd24a081721e0c48e99e6c220f2
mum <mumum3521@naver.com>
2024-06-08 02:06:14 +0900

    Feat : 힌트 생성 버튼 클릭시, 지점 및 테마 조회 기능 추가

--- xcape-admin/src/main/resources/static/js/hint-settings.js
+++ xcape-admin/src/main/resources/static/js/hint-settings.js
@@ -1,12 +1,12 @@
 const getHintList = (e) => {
     const id = e.currentTarget.dataset.themeId;
     axios.get(`/themes/${id}/hints`).then((res) => {
-        const {resultCode} = res.data;
+        const {resultCode, result} = res.data;
         if (resultCode === SUCCESS) {
             const tbody = document.querySelector('#hintTable tbody');
             const template = document.querySelector('#hintTemplate').innerHTML;
 
-            tbody.innerHTML = res.data.result.map(hint => {
+            tbody.innerHTML = result.map(hint => {
                 return template
                     .replaceAll('{id}', hint.id)
                     .replaceAll('{code}', hint.code)
@@ -19,7 +19,7 @@ const getHintList = (e) => {
 }
 
 document.querySelectorAll('#treeArea .accordion-body button').forEach((button) => {
-    button.addEventListener('click', getHintList)
+    button.addEventListener('click', getHintList);
 });
 
 // 테마 목록 클릭 시, active 추가
@@ -27,5 +27,44 @@ document.querySelectorAll('.list-group button').forEach(list => {
     list.addEventListener('click', () => {
         document.querySelector('.list-group .active')?.classList.remove('active');
         list.classList.add('active');
-    })
-})
+    });
+});
+
+document.querySelector('#merchantSelect').addEventListener('change', function () {
+    const merchantId = this.value;
+    const themeSelect = document.querySelector('#themeSelect');
+
+    themeSelect.innerHTML = '';
+
+    const template = document.querySelector('#themeOptionTemplate');
+    const clone = document.importNode(template.content, true);
+    themeSelect.appendChild(clone);
+
+    if (merchantId) {
+        axios.get(`/merchants/${merchantId}/themes`).then((res) => {
+            const {resultCode} = res.data;
+            if (resultCode === SUCCESS) {
+                res.data.result.forEach(theme => {
+                    let option = document.createElement('option');
+                    option.value = theme.id;
+                    option.textContent = theme.nameKo;
+                    themeSelect.appendChild(option);
+                });
+            }
+        });
+    }
+});
+
+// 힌트 만들기 버튼 클릭 시, 지점 및 테마 셀렉트 박스 초기화
+document.querySelector('#hintCreateModalButton').addEventListener('click', () => {
+    const merchantSelect = document.querySelector('#merchantSelect');
+    const themeSelect = document.querySelector('#themeSelect');
+
+    merchantSelect.selectedIndex = 0;
+    themeSelect.selectedIndex = 0;
+
+    const template = document.querySelector('#themeOptionTemplate');
+    const clone = document.importNode(template.content, true);
+    themeSelect.innerHTML = '';
+    themeSelect.appendChild(clone);
+});
