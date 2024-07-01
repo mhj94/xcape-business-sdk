d3b5b7bb9564a43d6d1cea4dc85fe80ae0af9059
mum <mumum3521@naver.com>
2024-07-01 16:34:04 +0900

    Fix : 태그 발행 병렬 처리 수정

--- xcape-admin/src/main/resources/static/js/tag-settings.js
+++ xcape-admin/src/main/resources/static/js/tag-settings.js
@@ -68,42 +68,91 @@ document.querySelector('#themeSelect').addEventListener('change', (e) => {
     }
 });
 
-// 태그 및 뷰 발행
-document.querySelector('#jsonPublishButton').addEventListener('click', () => {
-    Promise.all([
-        axios.get('/tags'),
-        axios.get('/views')
-    ])
-        .then(([tagsResponse, viewResponse]) => {
-            const {resultCode: tagsResultCode, result: tagList} = tagsResponse.data;
-            const {resultCode: viewResultCode, result: viewList} = viewResponse.data;
+// 발행
+// document.querySelector('#jsonPublishButton').addEventListener('click', () => {
+//     Promise.all([
+//         axios.get('/tags'),
+//         axios.get('/views')
+//     ])
+//         .then(([tagsResponse, viewResponse]) => {
+//             const {resultCode: tagsResultCode, result: tagList} = tagsResponse.data;
+//             const {resultCode: viewResultCode, result: viewList} = viewResponse.data;
+//
+//             if (tagsResultCode === SUCCESS && viewResultCode === SUCCESS) {
+//                 // 태그 파일 업로드
+//                 let tagForm = new FormData();
+//                 tagForm.append('file', new File([JSON.stringify(tagList)], JSON_FILE_NAME));
+//                 tagForm.append('type', JSON_FILE_TYPE.TAG);
+//
+//                 // 뷰 파일 업로드
+//                 let viewForm = new FormData();
+//                 viewForm.append('file', new File([JSON.stringify(viewList)], JSON_FILE_NAME));
+//                 viewForm.append('type', JSON_FILE_TYPE.VIEW);
+//
+//                 const uploadTag = axios.put('/json', tagForm);
+//                 const uploadView = axios.put('/json', viewForm);
+//
+//                 return Promise.all([uploadTag, uploadView])
+//                     .then(([tagsResponse, viewResponse]) => {
+//                         const tagPath = tagsResponse.data;
+//                         const viewPath = viewResponse.data;
+//                         if (tagPath && viewPath) {
+//                             alert(`태그 : ${tagPath} 및 view : ${viewPath}\n 발행 완료되었습니다.`);
+//                             return;
+//                         }
+//                         alert('발행에 실패했습니다.');
+//                     });
+//             }
+//         });
+//
+// });
 
-            if (tagsResultCode === SUCCESS && viewResultCode === SUCCESS) {
-                // 태그 파일 업로드
-                let tagForm = new FormData();
-                tagForm.append('file', new File([JSON.stringify(tagList)], JSON_FILE_NAME));
-                tagForm.append('type', JSON_FILE_TYPE.TAG);
 
-                // 뷰 파일 업로드
-                let viewForm = new FormData();
-                viewForm.append('file', new File([JSON.stringify(viewList)], JSON_FILE_NAME));
-                viewForm.append('type', JSON_FILE_TYPE.VIEW);
+// 발행 버튼 클릭시 이벤트
+document.querySelector('#jsonPublishButton').addEventListener('click', async () => {
+    try {
+        const [tagResponse, viewResponse] = await Promise.all([
+            axios.get('/tags'),
+            axios.get('/views')
+        ]);
 
-                const uploadTag = axios.put('/json', tagForm);
-                const uploadView = axios.put('/json', viewForm);
-
-                return Promise.all([uploadTag, uploadView])
-                    .then(([tagsResponse, viewResponse]) => {
-                        const tagPath = tagsResponse.data;
-                        const viewPath = viewResponse.data;
-                        if (tagPath && viewPath) {
-                            alert(`태그 : ${tagPath} 및 view : ${viewPath}\n 발행 완료되었습니다.`);
-                            return;
-                        }
-                        alert('발행에 실패했습니다.');
-                    });
-            }
-        });
-
+        await handleDataResponse(tagResponse, viewResponse);
+    } catch (error) {
+        alert('발행에 실패했습니다.');
+    }
 });
 
+const handleDataResponse = async (tagResponse, viewResponse) => {
+    const {resultCode: tagResultCode, result: tagResult} = tagResponse.data;
+    const {resultCode: viewResultCode, result: viewResult} = viewResponse.data;
+
+    if (tagResultCode !== SUCCESS || viewResultCode !== SUCCESS) {
+        throw new Error('조회 실패');
+    }
+
+    // 병렬 처리
+    // const [tagUploadResponse, viewUploadResponse] = await Promise.all([
+    //     uploadJsonFile(tagResult, JSON_FILE_TYPE.TAG),
+    //     uploadJsonFile(viewResult, JSON_FILE_TYPE.VIEW)
+    // ]);
+
+    const tagUploadResponse = await uploadJsonFile(tagResult, JSON_FILE_TYPE.TAG);
+    const viewUploadResponse = await uploadJsonFile(viewResult, JSON_FILE_TYPE.VIEW);
+
+    const tagPath = tagUploadResponse.data;
+    const viewPath = viewUploadResponse.data;
+
+    if (!tagPath || !viewPath) {
+        throw new Error('파일 업로드 실패');
+    }
+    alert(`태그 : ${tagPath} 및 뷰 : ${viewPath}\n 발행이 완료되었습니다.`);
+};
+
+// 업로드
+const uploadJsonFile = async (data, type) => {
+    let form = new FormData();
+    form.append('file', new File([JSON.stringify(data)], JSON_FILE_NAME));
+    form.append('type', type);
+
+    return await axios.put('/json', form);
+};
